83e40197f99a6ddd1dd2320241f6ea4f5d154570
Ken <rkenmi@gmail.com>
2016-03-28 23:06:03 -0700

    use psycopg2 for db

--- vtuner/settings.py
+++ vtuner/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'vtuner.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'dqa2ktl5s3sfc',
         'USER': 'luusoqdwqfqeyc',
         'PASSWORD': 'Jv6wF-VVIucrZLEofCpKNNJUdL',
