cc0b9a2a2b94a2de7245285488ff1397a278b81d
Ken <rkenmi@gmail.com>
2016-04-06 21:43:20 -0700

    add file verification

--- web/vtunerRG/driver.py
+++ web/vtunerRG/driver.py
@@ -1,7 +1,8 @@
 from io import TagReadWrite
-from calc_gain import CalcGain
+from calc_gain import *
 
 def ReplayGain(filepath):
     tags = TagReadWrite(filepath)
-    new_tags_data = CalcGain(filepath)
+    new_tags_data = calc_gain(filepath)
     tags.write_obj(filepath, new_tags_data[0])
+    return new_tags_data[0]
